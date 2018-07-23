bcea4e28faeaa7eef48825cfef616591a6e4ed6c
Steve <>
2018-07-23 11:37:09 -0400

    .

--- Exercise Files/Ch02/02_01/start/dist/index.js
+++ Exercise Files/Ch02/02_01/start/dist/index.js
@@ -1,6 +1,12 @@
 const { createElement} = React
 const { render } = ReactDOM
 
+const style = {
+	backgroundColor: 'orange',
+	color: 'white',
+	fontFamily: 'verdana'
+}
+
 const title = createElement(
 	'h1',
 	{id: 'title', className: 'header'},
